452521d1d8a59529cfb8c409200fd4c1b1a0bb49
Jaderson Morais <jaderson.morais@sulwork.com.br>
2025-10-18 21:23:21 +0100

    Add columnn users and first login.

--- src/main/resources/db/migration/V32__202510152100_add_first_access_flag.sql
+++ src/main/resources/db/migration/V32__202510152100_add_first_access_flag.sql
@@ -1,3 +1,10 @@
-ALTER TABLE user_entity ADD COLUMN first_access BOOLEAN DEFAULT TRUE;
+ALTER TABLE user_entity ADD COLUMN IF NOT EXISTS first_access BOOLEAN DEFAULT TRUE;
 
 UPDATE user_entity SET first_access = FALSE WHERE first_access IS NULL;
+
+ALTER TABLE user_entity ADD COLUMN IF NOT EXISTS locale VARCHAR(10) DEFAULT 'en_US';
+
+UPDATE user_entity SET locale = 'en_US' WHERE locale IS NULL;
+
+ALTER TABLE user_entity ADD COLUMN IF NOT EXISTS password_recovery_token VARCHAR(200) DEFAULT NULL;
+ALTER TABLE user_entity ADD COLUMN IF NOT EXISTS password_recovery_token_expires_at VARCHAR(200) DEFAULT NULL;
