9ef83f7f559f324cd9102c899e6aae06dfe34c55
Jaderson Morais <jaderson.morais@gmail.com>
2025-10-18 23:23:56 +0100

    Improve landing subscription modal and carousel

---
+++ src/main/java/com/jm/dto/WhatsAppRegisterDTO.java
@@ -0,0 +1,17 @@
+package com.jm.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WhatsAppRegisterDTO {
+
+    @NotNull
+    private UUID userId;
+}
